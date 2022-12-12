98b540d222ffd27cd1778cb2fe3557f9cd892297
Julia Ch <juliecha@yandex.ru>
2022-12-12 21:50:18 +0300

    Add constructions.py

---
+++ Lesson_1/constructions.py
@@ -0,0 +1,12 @@
+# Управляющие конструкции: if, if-else
+# if condition:
+#  # operator 1
+#  # operator 2
+#  # ...
+#  # operator n
+# else:
+#  # operator n + 1
+#  # operator n + 2
+#  # ...
+#  # operator n + m
+
