9d63c88b729e4ea5383c55df9ac0216fed7afac2
Julia Ch <juliecha@yandex.ru>
2022-12-18 20:32:51 +0300

    Add tuple

---
+++ Lesson_2/tuple.py
@@ -0,0 +1,24 @@
+t = ()
+print(type(t)) # tuple
+t = (1,)
+print(type(t)) # tuple
+t = (1)
+print(type(t)) # int
+t = (28, 9, 1990)
+print(type(t)) # tuple
+colors = ['red', 'green', 'blue']
+print(colors) # ['red', 'green', 'blue']
+t = tuple(colors)
+print(t) # ('red', 'green', 'blue')
+
+
+
+a, b = 3, 4
+print(a, b)
+
+c = (3, 4, 4, 6, 7)
+print(c)
+print(c[0])
+print(c[-1]) # последний элемент
+print(c[-2]) # предпоследний элемент
+# для картежей не работает присваивание с[0] = 12
