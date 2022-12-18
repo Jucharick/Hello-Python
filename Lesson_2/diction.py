d7ca10f7fdf950f5543d5bdedcbb02cbc54ff958
Julia Ch <juliecha@yandex.ru>
2022-12-18 20:46:09 +0300

    UPD dictionary

--- Lesson_2/diction.py
+++ Lesson_2/diction.py
@@ -9,3 +9,12 @@ dictionary = \
 print(dictionary) # {'up':'↑', 'left':'←', 'down':'↓', 'right':'→'}
 print(dictionary['left']) # ←
 # типы ключей могут отличаться
+
+for k in dictionary.keys():
+    print(k)
+
+for v in dictionary.values():
+    print(v)
+
+for i in dictionary.items():
+    print(i)
