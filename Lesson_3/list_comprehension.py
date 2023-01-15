f1c8dbfee0ac97e6c2113eb52642a35f692d3b65
Julia Ch <juliecha@yandex.ru>
2023-01-15 13:33:21 +0300

    UPD list_comprehension

---
+++ Lesson_3/list_comprehension.py
@@ -0,0 +1,18 @@
+list = []
+
+# for i in range(1, 21):
+#       if(i%2 == 0):
+#           list.append(i)
+# print(list)
+
+list = [i for i in range(1, 21) if(i%2 == 0)] # второй тип записи добавления элементов в список list
+print(list)
+
+list1 = [(i, i) for i in range(1, 21) if(i%2 == 0)] 
+print(list1)
+
+def f(x):
+    return x**3
+
+list2 = [f(i) for i in range(1, 21) if(i%2 == 0)] 
+print(list2)
