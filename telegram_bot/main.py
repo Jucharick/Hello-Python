336c82947b0f3fabcfe8f1df1516a07367f8b6d0
Julia Ch <juliecha@yandex.ru>
2023-01-26 23:15:01 +0300

    Add telegram_bot

---
+++ telegram_bot/main.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+
+app = ApplicationBuilder().token("5809739102:AAEzOgJLIZeNWOfin-gmRTvnyeBFjmidbiM").build()
+
+app.add_handler(CommandHandler("Hi", hi_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
+print('server start')
+app.run_polling()
